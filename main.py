4cd81ebeeef61e66a5af20a5b76441a859c9ae6c
Rittik <rittikkumar156@gmail.com>
2026-06-10 22:28:02 +0600

    feat: implement in-memory CRUD operations for task manager API

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI 
+
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 app=FastAPI() # Here app is server application which cover all things
 
-class Takeschema(BaseModel):
+class TakeSchema(BaseModel):
     id:int
     text:str
     completed:bool=False
@@ -13,7 +14,7 @@ todo_list=[{"id":1,"text":"learn fastapi","completed":False},
 def get_all_tasks():
     return{"status":"success","data":todo_list}
 @app.post("/tasks")
-def create_task(new_task:Takeschema):
+def create_task(new_task:TakeSchema):
     take_dict=new_task.model_dump()
     for item in todo_list:
         if item["id"] == take_dict["id"]:
@@ -21,3 +22,20 @@ def create_task(new_task:Takeschema):
             return {"status": "error", "message": f"Task with ID {take_dict['id']} already exists!"}
     todo_list.append(take_dict)
     return {"status submitted":"success"," added data":take_dict}
+@app.put("/tasks/{new_id}")
+def update_task(new_id:int,updated_task:TakeSchema):
+    for item in todo_list:
+        if item["id"]==new_id:
+            item["text"]=updated_task.text
+            item["completed"]=updated_task.completed
+            return {"status":"update success","updated_data":item}
+    raise HTTPException(status_code=404,detail="text not found")
+
+
+@app.delete("/tasks/{new_id}")
+def delete_task(new_id:int):
+    for item in todo_list:
+        if item["id"]==new_id:
+            todo_list.remove(item)
+            return { "status":"delete success","message":f"Task with ID {new_id}"}
+    raise HTTPException(status_code=404,detail="task not found")    
