b854017a66a65af51ef98c9bbafa021d90182c58
Rittik <rittikkumar156@gmail.com>
2026-06-13 13:24:10 +0600

    feat: implement full CRUD operations and query parameter search

--- main.py
+++ main.py
@@ -3,25 +3,31 @@ from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 app=FastAPI() # Here app is server application which cover all things
 
-class TakeSchema(BaseModel):
+#Here i implemented all HTTP methods(get,post,put,delete)//CRUD
+
+class TakeSchema(BaseModel): #Data validation using pydantic library
     id:int
     text:str
     completed:bool=False
 todo_list=[{"id":1,"text":"learn fastapi","completed":False},
       {"id":2,"text":"Run server","completed":True}
 ]    
-@app.get("/tasks")  # Decorator + Path + HTTP Method
+@app.get("/tasks")  # Decorator + HTTP Method + Path
 def get_all_tasks():
     return{"status":"success","data":todo_list}
+
+
 @app.post("/tasks")
 def create_task(new_task:TakeSchema):
     take_dict=new_task.model_dump()
     for item in todo_list:
         if item["id"] == take_dict["id"]:
             
-            return {"status": "error", "message": f"Task with ID {take_dict['id']} already exists!"}
+            return {"status": "error", "message": f"Task with ID {take_dict['id']} already exists!"} #format string that contain variable
     todo_list.append(take_dict)
     return {"status submitted":"success"," added data":take_dict}
+
+
 @app.put("/tasks/{new_id}")
 def update_task(new_id:int,updated_task:TakeSchema):
     for item in todo_list:
@@ -29,7 +35,7 @@ def update_task(new_id:int,updated_task:TakeSchema):
             item["text"]=updated_task.text
             item["completed"]=updated_task.completed
             return {"status":"update success","updated_data":item}
-    raise HTTPException(status_code=404,detail="text not found")
+    raise HTTPException(status_code=404,detail="text not found") #Error Handling
 
 
 @app.delete("/tasks/{new_id}")
@@ -38,4 +44,18 @@ def delete_task(new_id:int):
         if item["id"]==new_id:
             todo_list.remove(item)
             return { "status":"delete success","message":f"Task with ID {new_id}"}
-    raise HTTPException(status_code=404,detail="task not found")    
+    raise HTTPException(status_code=404,detail="task not found")   
+
+
+
+#Query Parameters for searching
+@app.get("/tasksfind")
+def get_search_tasks(search: str=None): # search:str is a type hint
+    if not search:
+        return {"status":"success","data":todo_list}
+    filter_task=[]
+    for item in todo_list:
+        if search.lower() in item["text"].lower():
+            filter_task.append(item)
+        
+    return {"status":"success","filtered data":filter_task}
